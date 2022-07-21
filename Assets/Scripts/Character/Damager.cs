2e076a907521e3af6770f59886c6bf4281be26d6
Pietro <pietro.digiovanni.dev@gmail.com>
2022-07-21 11:45:48 +0200

    disable componets of character on drag, enabled when is spawn on tile

--- Assets/Scripts/Character/Damager.cs
+++ Assets/Scripts/Character/Damager.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class Damager : Character
 {
     [SerializeField] int damage;
+    [SerializeField] float firingDistance;
 
-    private void Start()
+    private void Update()
     {
-        Debug.Log("Damage " + damage);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right,firingDistance, LayerMask.GetMask("Enemy"));
+
+        if(hit.collider != null)
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Dealing " + damage + " damage");
     }
 }
